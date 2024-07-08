e0e7d3182536d41c4d70ad4246e8b3a2c9070efd
Tyh2001 <1469442737@qq.com>
2024-07-08 17:37:27 +0800

    feat:

--- sql/city_walk.sql
+++ sql/city_walk.sql
@@ -3,15 +3,15 @@
 
  Source Server         : city_walk
  Source Server Type    : MySQL
- Source Server Version : 80031
+ Source Server Version : 80300 (8.3.0)
  Source Host           : localhost:3306
  Source Schema         : city_walk
 
  Target Server Type    : MySQL
- Target Server Version : 80031
+ Target Server Version : 80300 (8.3.0)
  File Encoding         : 65001
 
- Date: 08/07/2024 15:28:31
+ Date: 08/07/2024 17:36:57
 */
 
 SET NAMES utf8mb4;
@@ -31,6 +31,12 @@ CREATE TABLE `cw_user_friend_invite` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
+-- Records of cw_user_friend_invite
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
 -- Table structure for cw_user_friend_relationship
 -- ----------------------------
 DROP TABLE IF EXISTS `cw_user_friend_relationship`;
@@ -42,6 +48,12 @@ CREATE TABLE `cw_user_friend_relationship` (
   `created_at` varchar(255) NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of cw_user_friend_relationship
+-- ----------------------------
+BEGIN;
+COMMIT;
 
 -- ----------------------------
 -- Table structure for cw_user_info
@@ -70,59 +82,13 @@ CREATE TABLE `cw_user_info` (
 -- Records of cw_user_info
 -- ----------------------------
 BEGIN;
-INSERT INTO `cw_user_info` VALUES (1, '', '_1469442737@qq.com', '15211111111', '最怕你一生碌碌无为，还安慰自己平凡可贵。', '杭州市', '2023-09-26 15:31:52', '2001-01-03', '男', '田', '/uploads/avatar/2024-05-15/1715748167980-702831789.jpg', NULL, NULL, '浙江省', NULL);
-INSERT INTO `cw_user_info` VALUES (2, '', 'haiyetianguoqiang@126.com', NULL, NULL, NULL, '2023-10-01 21:57:09', NULL, NULL, '强', '/uploads/avatar/2023-10-01/1696168654117-483818004.jpg', NULL, NULL, NULL, NULL);
-INSERT INTO `cw_user_info` VALUES (3, '', '1050527857@qq.com', NULL, NULL, NULL, '2023-10-01 21:58:56', NULL, NULL, '欢欢乐乐', '/uploads/avatar/2023-10-01/1696168799735-403821510.jpg', NULL, NULL, NULL, NULL);
-INSERT INTO `cw_user_info` VALUES (4, '', '542219987@qq.com', NULL, NULL, NULL, '2023-11-02 10:33:15', NULL, NULL, '2', '/uploads/avatar/2023-11-02/1698892442746-825618819.jpg', NULL, NULL, NULL, NULL);
-INSERT INTO `cw_user_info` VALUES (5, '111', '1741342007@qq.com', NULL, NULL, NULL, '2023-11-16 08:27:42', NULL, NULL, '维', '/uploads/avatar/2023-11-16/1700094491342-260377542.jpg', NULL, NULL, NULL, NULL);
-INSERT INTO `cw_user_info` VALUES (31, 'U300102695417628064381040124321866117463', '_1469442737@qq.com', NULL, NULL, NULL, '2024-06-27 21:38:25', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `cw_user_info` VALUES (32, 'U131995175454824711531011225172573302849', '1469442737@qq.com', '15211111111', '等好久啊', NULL, '2024-06-27 22:14:30', NULL, NULL, '等好久啊好激动', NULL, NULL, NULL, NULL, NULL);
-COMMIT;
-
--- ----------------------------
--- Table structure for cw_user_province_experience
--- ----------------------------
-DROP TABLE IF EXISTS `cw_user_province_experience`;
-CREATE TABLE `cw_user_province_experience` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `province_code` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '省份编码',
-  `user_id` int NOT NULL COMMENT '用户 id',
-  `created_at` datetime NOT NULL COMMENT '创建时间',
-  `value` int NOT NULL COMMENT '经验值',
-  `get_method` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '获取方式',
-  `is_collect` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '是否收集 0 -> 未收集 1 -> 已收集',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_danish_ci ROW_FORMAT=DYNAMIC;
-
--- ----------------------------
--- Records of cw_user_province_experience
--- ----------------------------
-BEGIN;
-INSERT INTO `cw_user_province_experience` VALUES (1, '330000', 1, '2023-11-09 20:07:36', 20, 'article', '1');
-INSERT INTO `cw_user_province_experience` VALUES (2, '330000', 1, '2023-11-09 20:09:06', 20, 'article', '1');
-INSERT INTO `cw_user_province_experience` VALUES (3, '330000', 1, '2023-11-09 20:09:25', 20, 'article', '1');
-INSERT INTO `cw_user_province_experience` VALUES (4, '330000', 1, '2023-11-12 17:59:08', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (5, '330000', 1, '2023-11-12 18:31:42', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (6, '330000', 1, '2023-11-12 18:31:57', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (7, '330000', 1, '2023-11-13 08:48:24', 20, 'article', '1');
-INSERT INTO `cw_user_province_experience` VALUES (8, '330000', 1, '2023-11-13 12:33:28', 20, 'article', '1');
-INSERT INTO `cw_user_province_experience` VALUES (9, '330000', 1, '2023-11-13 12:33:48', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (10, '330000', 1, '2023-11-13 12:46:25', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (11, '330000', 1, '2023-11-13 12:46:31', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (12, '330000', 1, '2023-11-13 12:47:06', 20, 'article', '1');
-INSERT INTO `cw_user_province_experience` VALUES (13, '330000', 1, '2023-11-13 12:47:43', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (14, '330000', 1, '2023-11-13 12:51:25', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (15, '330000', 1, '2023-11-13 12:51:36', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (16, '330000', 1, '2023-11-13 12:52:30', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (17, '330000', 5, '2023-11-15 22:30:58', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (18, '330000', 5, '2023-11-16 08:30:16', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (19, '330000', 5, '2023-11-16 08:32:06', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (20, '330000', 5, '2023-11-16 08:38:33', 5, 'clock_location', '0');
-INSERT INTO `cw_user_province_experience` VALUES (21, '330000', 5, '2023-11-16 12:08:36', 5, 'clock_location', '0');
-INSERT INTO `cw_user_province_experience` VALUES (22, '330000', 5, '2023-11-16 12:12:43', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (23, '330000', 6, '2023-11-16 18:59:33', 5, 'clock_location', '0');
-INSERT INTO `cw_user_province_experience` VALUES (24, '330000', 6, '2023-11-16 19:01:27', 5, 'clock_location', '1');
-INSERT INTO `cw_user_province_experience` VALUES (25, '330000', 1, '2024-01-12 22:06:59', 20, 'article', '0');
+INSERT INTO `cw_user_info` (`id`, `user_id`, `email`, `mobile`, `signature`, `city`, `created_at`, `birthday`, `gender`, `nick_name`, `avatar`, `ip_address`, `ip_info`, `province`, `preference_type`) VALUES (1, '', '_1469442737@qq.com', '15211111111', '最怕你一生碌碌无为，还安慰自己平凡可贵。', '杭州市', '2023-09-26 15:31:52', '2001-01-03', '男', '田', '/uploads/avatar/2024-05-15/1715748167980-702831789.jpg', NULL, NULL, '浙江省', NULL);
+INSERT INTO `cw_user_info` (`id`, `user_id`, `email`, `mobile`, `signature`, `city`, `created_at`, `birthday`, `gender`, `nick_name`, `avatar`, `ip_address`, `ip_info`, `province`, `preference_type`) VALUES (2, '', 'haiyetianguoqiang@126.com', NULL, NULL, NULL, '2023-10-01 21:57:09', NULL, NULL, '强', '/uploads/avatar/2023-10-01/1696168654117-483818004.jpg', NULL, NULL, NULL, NULL);
+INSERT INTO `cw_user_info` (`id`, `user_id`, `email`, `mobile`, `signature`, `city`, `created_at`, `birthday`, `gender`, `nick_name`, `avatar`, `ip_address`, `ip_info`, `province`, `preference_type`) VALUES (3, '', '1050527857@qq.com', NULL, NULL, NULL, '2023-10-01 21:58:56', NULL, NULL, '欢欢乐乐', '/uploads/avatar/2023-10-01/1696168799735-403821510.jpg', NULL, NULL, NULL, NULL);
+INSERT INTO `cw_user_info` (`id`, `user_id`, `email`, `mobile`, `signature`, `city`, `created_at`, `birthday`, `gender`, `nick_name`, `avatar`, `ip_address`, `ip_info`, `province`, `preference_type`) VALUES (4, '', '542219987@qq.com', NULL, NULL, NULL, '2023-11-02 10:33:15', NULL, NULL, '2', '/uploads/avatar/2023-11-02/1698892442746-825618819.jpg', NULL, NULL, NULL, NULL);
+INSERT INTO `cw_user_info` (`id`, `user_id`, `email`, `mobile`, `signature`, `city`, `created_at`, `birthday`, `gender`, `nick_name`, `avatar`, `ip_address`, `ip_info`, `province`, `preference_type`) VALUES (5, '111', '1741342007@qq.com', NULL, NULL, NULL, '2023-11-16 08:27:42', NULL, NULL, '维', '/uploads/avatar/2023-11-16/1700094491342-260377542.jpg', NULL, NULL, NULL, NULL);
+INSERT INTO `cw_user_info` (`id`, `user_id`, `email`, `mobile`, `signature`, `city`, `created_at`, `birthday`, `gender`, `nick_name`, `avatar`, `ip_address`, `ip_info`, `province`, `preference_type`) VALUES (31, 'U300102695417628064381040124321866117463', '_1469442737@qq.com', NULL, NULL, NULL, '2024-06-27 21:38:25', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `cw_user_info` (`id`, `user_id`, `email`, `mobile`, `signature`, `city`, `created_at`, `birthday`, `gender`, `nick_name`, `avatar`, `ip_address`, `ip_info`, `province`, `preference_type`) VALUES (32, 'U131995175454824711531011225172573302849', '1469442737@qq.com', '15211111111', '等好久啊', NULL, '2024-06-27 22:14:30', NULL, NULL, '等好久啊好激动', NULL, NULL, NULL, NULL, NULL);
 COMMIT;
 
 -- ----------------------------
@@ -131,8 +97,8 @@ COMMIT;
 DROP TABLE IF EXISTS `cw_user_route`;
 CREATE TABLE `cw_user_route` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `list_id` varchar(255) COLLATE utf8mb3_danish_ci NOT NULL COMMENT '列表 id',
-  `user_id` varchar(255) COLLATE utf8mb3_danish_ci NOT NULL COMMENT '用户 id',
+  `list_id` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '列表 id',
+  `user_id` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '用户 id',
   `province_code` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '省份编码',
   `create_at` datetime NOT NULL COMMENT '创建时间',
   `city` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '当前城市',
@@ -142,7 +108,22 @@ CREATE TABLE `cw_user_route` (
   `longitude` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '经度',
   `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '地点名称',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_danish_ci ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_danish_ci ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of cw_user_route
+-- ----------------------------
+BEGIN;
+INSERT INTO `cw_user_route` (`id`, `list_id`, `user_id`, `province_code`, `create_at`, `city`, `province`, `address`, `latitude`, `longitude`, `name`) VALUES (1, 'RO220454790246139943250231328418604080958', 'U131995175454824711531011225172573302849', '330000', '2024-07-08 16:02:24', '舟山市', '浙江省', '', '30.709778', '122.455646', '');
+INSERT INTO `cw_user_route` (`id`, `list_id`, `user_id`, `province_code`, `create_at`, `city`, `province`, `address`, `latitude`, `longitude`, `name`) VALUES (2, 'RO220454790246139943250231328418604080958', 'U131995175454824711531011225172573302849', '330000', '2024-07-08 16:03:48', '湖州市', '浙江省', '', '30.709778', '120.455646', '');
+INSERT INTO `cw_user_route` (`id`, `list_id`, `user_id`, `province_code`, `create_at`, `city`, `province`, `address`, `latitude`, `longitude`, `name`) VALUES (3, 'RO220454790246139943250231328418604080958', 'U131995175454824711531011225172573302849', '330000', '2024-07-08 16:04:58', '湖州市', '浙江省', '', '30.709778', '120.1', '');
+INSERT INTO `cw_user_route` (`id`, `list_id`, `user_id`, `province_code`, `create_at`, `city`, `province`, `address`, `latitude`, `longitude`, `name`) VALUES (4, 'RO220454790246139943250231328418604080958', 'U131995175454824711531011225172573302849', '310000', '2024-07-08 16:07:51', '未知城市', '上海市', '', '30.709778', '121.3333', '');
+INSERT INTO `cw_user_route` (`id`, `list_id`, `user_id`, `province_code`, `create_at`, `city`, `province`, `address`, `latitude`, `longitude`, `name`) VALUES (5, 'RO220454790246139943250231328418604080958', 'U131995175454824711531011225172573302849', '310000', '2024-07-08 16:08:19', '未知城市', '上海市', '', '30.709778', '121.3333', '');
+INSERT INTO `cw_user_route` (`id`, `list_id`, `user_id`, `province_code`, `create_at`, `city`, `province`, `address`, `latitude`, `longitude`, `name`) VALUES (6, 'RO220454790246139943250231328418604080958', 'U131995175454824711531011225172573302849', '310000', '2024-07-08 16:09:04', '未知城市', '上海市', '', '30.709778', '121.3333', '');
+INSERT INTO `cw_user_route` (`id`, `list_id`, `user_id`, `province_code`, `create_at`, `city`, `province`, `address`, `latitude`, `longitude`, `name`) VALUES (7, 'RO220454790246139943250231328418604080958', 'U131995175454824711531011225172573302849', '310000', '2024-07-08 16:10:20', '未知城市', '上海市', '', '30.709778', '121.3333', '');
+INSERT INTO `cw_user_route` (`id`, `list_id`, `user_id`, `province_code`, `create_at`, `city`, `province`, `address`, `latitude`, `longitude`, `name`) VALUES (8, 'RO220454790246139943250231328418604080958', 'U131995175454824711531011225172573302849', '540000', '2024-07-08 16:10:29', '拉萨市', '西藏自治区', '', '30.709778', '91.3333', '');
+INSERT INTO `cw_user_route` (`id`, `list_id`, `user_id`, `province_code`, `create_at`, `city`, `province`, `address`, `latitude`, `longitude`, `name`) VALUES (9, 'RO220454790246139943250231328418604080958', 'U131995175454824711531011225172573302849', '540000', '2024-07-08 17:08:06', '拉萨市', '西藏自治区', '', '30.709778', '91.3333', '');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for cw_user_route_list
@@ -151,10 +132,17 @@ DROP TABLE IF EXISTS `cw_user_route_list`;
 CREATE TABLE `cw_user_route_list` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'id',
   `list_id` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '列表 id',
-  `user_id` varchar(255) COLLATE utf8mb3_danish_ci NOT NULL COMMENT '用户 id',
+  `user_id` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '用户 id',
   `create_at` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_danish_ci ROW_FORMAT=FIXED;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_danish_ci ROW_FORMAT=FIXED;
+
+-- ----------------------------
+-- Records of cw_user_route_list
+-- ----------------------------
+BEGIN;
+INSERT INTO `cw_user_route_list` (`id`, `list_id`, `user_id`, `create_at`) VALUES (2, 'RO220454790246139943250231328418604080958', 'U131995175454824711531011225172573302849', '2024-07-08 15:54:22');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for cw_user_visited_province
@@ -162,21 +150,23 @@ CREATE TABLE `cw_user_route_list` (
 DROP TABLE IF EXISTS `cw_user_visited_province`;
 CREATE TABLE `cw_user_visited_province` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `vis_id` varchar(255) COLLATE utf8mb3_danish_ci NOT NULL,
-  `user_id` varchar(255) COLLATE utf8mb3_danish_ci NOT NULL COMMENT '用户 id',
+  `vis_id` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL,
+  `user_id` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '用户 id',
   `province_code` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '省份编码',
   `province_name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_danish_ci NOT NULL COMMENT '省份名称',
   `experience_value` int NOT NULL COMMENT '访问 id',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_danish_ci ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_danish_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of cw_user_visited_province
 -- ----------------------------
 BEGIN;
-INSERT INTO `cw_user_visited_province` VALUES (2, 'VIS165577110277401578717087648494990815791', 'U131995175454824711531011225172573302849', '330000', '浙江省', 280);
-INSERT INTO `cw_user_visited_province` VALUES (3, 'VIS31641110319140132264666038695908363882', 'U131995175454824711531011225172573302849', '510000', '四川省', 40);
-INSERT INTO `cw_user_visited_province` VALUES (4, 'VIS169369425513711531359349849739489571299', 'U131995175454824711531011225172573302849', '420000', '湖北省', 360);
+INSERT INTO `cw_user_visited_province` (`id`, `vis_id`, `user_id`, `province_code`, `province_name`, `experience_value`) VALUES (2, 'VIS165577110277401578717087648494990815791', 'U131995175454824711531011225172573302849', '330000', '浙江省', 560);
+INSERT INTO `cw_user_visited_province` (`id`, `vis_id`, `user_id`, `province_code`, `province_name`, `experience_value`) VALUES (3, 'VIS31641110319140132264666038695908363882', 'U131995175454824711531011225172573302849', '510000', '四川省', 40);
+INSERT INTO `cw_user_visited_province` (`id`, `vis_id`, `user_id`, `province_code`, `province_name`, `experience_value`) VALUES (4, 'VIS169369425513711531359349849739489571299', 'U131995175454824711531011225172573302849', '420000', '湖北省', 360);
+INSERT INTO `cw_user_visited_province` (`id`, `vis_id`, `user_id`, `province_code`, `province_name`, `experience_value`) VALUES (5, 'VIS164511652947648828322693781494491035658', 'U131995175454824711531011225172573302849', '310000', '上海市', 140);
+INSERT INTO `cw_user_visited_province` (`id`, `vis_id`, `user_id`, `province_code`, `province_name`, `experience_value`) VALUES (6, 'VIS156624802457223725839386130664692004012', 'U131995175454824711531011225172573302849', '540000', '西藏自治区', 40);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
