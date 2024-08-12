c44b079b39dc95f31ee7740701d82503181cb712
Tyh2001 <1469442737@qq.com>
2024-08-12 14:42:47 +0800

    chore:

---
+++ run-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+COMMAND="npm run"
+ARGS="main:test"
+
+start_service() {
+    nohup $COMMAND $ARGS > output.log 2>&1 &
+    echo "Success"
+}
+
+stop_service() {
+    pkill -f "$COMMAND $ARGS" --signal SIGTERM
+    echo "Okay"
+}
+
+case "$1" in
+    start)
+        start_service
+        ;;
+    stop)
+        stop_service
+        ;;
+    *)
+        echo "method: $0 {start|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0
